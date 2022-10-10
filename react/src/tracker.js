b308c749eb7feb190c421fcb8b32646d84481cfc
Peter Perlepes <p.perlepes@gmail.com>
2022-10-10 10:11:49 +0300

    Add youtube video tracking page and plugin

--- react/src/tracker.js
+++ react/src/tracker.js
@@ -2,9 +2,10 @@ import { newTracker, trackPageView, enableActivityTracking } from "@snowplow/bro
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { FormTrackingPlugin } from '@snowplow/browser-plugin-form-tracking';
+import { YouTubeTrackingPlugin } from '@snowplow/browser-plugin-youtube-tracking';
 
 let tracker = newTracker('ns1', 'http://localhost:9090', {
-  plugins: [ FormTrackingPlugin() ]
+  plugins: [FormTrackingPlugin(), YouTubeTrackingPlugin()]
 });
 
 enableActivityTracking({
