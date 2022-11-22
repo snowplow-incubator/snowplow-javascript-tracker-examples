b54c517f7c296387d21487797ca42ea9d0071e2d
Matúš Tomlein <matus.tomlein@gmail.com>
2022-11-22 13:29:40 +0100

    Add emitter callback and set buffer size to 1 in NodeJS example

--- express/src/tracking/snowplow.ts
+++ express/src/tracking/snowplow.ts
@@ -1,5 +1,21 @@
-import { tracker, gotEmitter, HttpProtocol } from "@snowplow/node-tracker";
+import { tracker, gotEmitter, HttpProtocol, HttpMethod } from "@snowplow/node-tracker";
 
-const emitter = gotEmitter("0.0.0.0", HttpProtocol.HTTPS, 9090);
+const emitter = gotEmitter(
+  "0.0.0.0",
+  HttpProtocol.HTTPS,
+  9090,
+  HttpMethod.POST,
+  1, // buffer size – 1 means that each event is sent right away, without buffering
+  5,
+  undefined,
+  function (error, response) {
+    // Callback called for each request
+    if (error) {
+      console.log(error, "Request error");
+    } else {
+      console.log("Event Sent");
+    }
+  }
+);
 
 export const expressTracker = tracker(emitter, "myTracker", "myApp", false);
